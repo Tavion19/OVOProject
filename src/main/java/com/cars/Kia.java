73b9b0436d9b33aada8dcba1fcdef9fa3b0ff663
Tavion19 <tapetty19@gmail.com>
2024-11-25 15:46:43 -0500

    added git commands

--- src/main/java/com/cars/Kia.java
+++ src/main/java/com/cars/Kia.java
@@ -3,5 +3,20 @@ package com.cars;
 public class Kia {
     public static void main(String[] args) {
         System.out.println("ifasidfaboibv");
+
+        // --------------First time
+        //git init
+        //git add -A
+        //git commit -m "message"
+        //git branch -M main
+        //git remote add origin originLink
+        //git push -u origin main
+
+        //------------------------------
+        //Updating the project with changes
+        //  git status
+        //  git add -A
+        //  git commit -m "message"
+        //  git push -u origin main
     }
 }
